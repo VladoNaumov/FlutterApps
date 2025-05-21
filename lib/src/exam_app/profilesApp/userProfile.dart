ededbae4dd351f83a379ecf6dd0d3efd6e7ec0ba
umbre <umbrella.spb@hotmail.com>
2025-05-22 01:11:03 +0300

    refactoring

--- lib/src/exam_app/profilesApp/userProfile.dart
+++ lib/src/exam_app/profilesApp/userProfile.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
-// Главный виджет профиля пользователя
+/// Главный экран профиля пользователя со списком настроек
 class UserProfile extends StatelessWidget {
-
-  final List<MenuRowData> firstMenuRow = [
+  // Первая группа пунктов меню
+  final List<MenuRowData> firstMenuRow = const [
     MenuRowData(Icons.favorite_outline, 'Favorite'),
     MenuRowData(Icons.call, 'Call'),
     MenuRowData(Icons.computer, 'Devices'),
     MenuRowData(Icons.folder, 'Chat Folder'),
   ];
 
-  final List<MenuRowData> secondMenuRow = [
+  // Вторая группа пунктов меню
+  final List<MenuRowData> secondMenuRow = const [
     MenuRowData(Icons.notifications, 'Notifications'),
     MenuRowData(Icons.privacy_tip, 'Privacy'),
     MenuRowData(Icons.date_range, 'Calendar'),
@@ -22,32 +23,31 @@ class UserProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Settings'),
+        title: Text('Settings'), // Заголовок в верхней панели
       ),
-      body: SingleChildScrollView( // Добавлено прокручивание
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            UserInfo(),
-            SizedBox(height: 30),
-            MenuWidget(menuRow: firstMenuRow),
-            SizedBox(height: 30),
-            MenuWidget(menuRow: secondMenuRow),
-          ],
-        ),
+      body: ListView( // Прокручиваемый список
+        padding: EdgeInsets.symmetric(vertical: 30),
+        children: [
+          UserInfo(), // Блок с аватаром и информацией о пользователе
+          SizedBox(height: 30),
+          MenuWidget(menuRow: firstMenuRow), // Первая группа настроек
+          SizedBox(height: 30),
+          MenuWidget(menuRow: secondMenuRow), // Вторая группа настроек
+        ],
       ),
     );
   }
 }
 
+/// Модель пункта меню: иконка + текст
 class MenuRowData {
   final IconData icon;
   final String text;
 
-  MenuRowData(this.icon, this.text);
+  const MenuRowData(this.icon, this.text);
 }
 
+/// Виджет для отображения группы пунктов меню
 class MenuWidget extends StatelessWidget {
   final List<MenuRowData> menuRow;
 
@@ -55,15 +55,16 @@ class MenuWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      itemCount: menuRow.length,
-      itemBuilder: (context, index) => MenuWidgetRow(data: menuRow[index]),
+    return Column(
+      children: [
+        for (final data in menuRow)
+          MenuWidgetRow(data: data),
+      ],
     );
   }
 }
 
+/// Один элемент меню
 class MenuWidgetRow extends StatelessWidget {
   final MenuRowData data;
 
@@ -71,92 +72,52 @@ class MenuWidgetRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Icon(data.icon),
-          SizedBox(width: 15),
-          Expanded(child: Text(data.text)),
-          Icon(Icons.chevron_right_sharp),
-        ],
-      ),
+    return ListTile(
+      leading: Icon(data.icon), // Иконка слева
+      title: Text(data.text), // Текст посередине
+      trailing: Icon(Icons.chevron_right_sharp), // Стрелка справа
+      contentPadding: EdgeInsets.symmetric(horizontal: 16),
+      onTap: () {
+        // Здесь можно обработать нажатие
+      },
     );
   }
 }
 
+/// Блок с информацией о пользователе
 class UserInfo extends StatelessWidget {
   const UserInfo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 30),
-      child: Column(
-        children: [
-          AvatarWidget(),
-          SizedBox(height: 20),
-          UserNameWidget(),
-          SizedBox(height: 10),
-          UserPhoneWidget(),
-          SizedBox(height: 10),
-          UserNickNameWidget(),
-        ],
-      ),
+    return Column(
+      children: [
+        // Аватар пользователя
+        CircleAvatar(
+          radius: 50,
+          backgroundColor: Colors.grey[300],
+          child: Icon(Icons.person, size: 50, color: Colors.white),
+        ),
+        SizedBox(height: 20),
+
+        // Имя пользователя
+        Text(
+          'Naum',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+
+        // Телефон пользователя
+        Text(
+          '+358 00 00 00',
+          style: TextStyle(color: Colors.grey[600]),
+        ),
+        SizedBox(height: 10),
+
+        // Никнейм пользователя
+        Text('@naumdeveloper'),
+      ],
     );
   }
 }
 
-class UserNickNameWidget extends StatelessWidget {
-  const UserNickNameWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('@naumdeveloper');
-  }
-}
-
-class UserPhoneWidget extends StatelessWidget {
-  const UserPhoneWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      '+358 00 00 00',
-      style: TextStyle(
-          color: Colors.grey[600]),
-    );
-  }
-}
-
-class UserNameWidget extends StatelessWidget {
-  const UserNameWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-        'Naum',
-        style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold
-    )
-    );
-  }
-}
-
-class AvatarWidget extends StatelessWidget {
-  const AvatarWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return CircleAvatar(
-      radius: 50,
-      backgroundColor: Colors.grey[300],
-      child: Icon(
-          Icons.person,
-          size: 50,
-          color: Colors.white),
-    );
-  }
-}
